baa5195418cf4985609a5f572a3862e6c5c8d41b
KityLinn <87649871+KityLinn@users.noreply.github.com>
2023-11-13 20:46:34 +0100

    added 6 seconds of wait

--- cypress/e2e/testing/login.cy.js
+++ cypress/e2e/testing/login.cy.js
@@ -4,23 +4,25 @@ import "dotenv/config";
 
 const url = "https://kitylinn.github.io/social-media-client/";
 
+const wait = 6000;
+
 describe("LOGING TESTING", () => {
   it("logs in and checks profile", () => {
     cy.visit(url);
-    cy.wait(1000);
+    cy.wait(wait);
     cy.get("#registerForm > .modal-footer > .btn.btn-outline-success").click();
-    cy.wait(1000);
+    cy.wait(wait);
     cy.get("#loginEmail").type(Cypress.env("LOGINEMAIL"));
     cy.get("#loginPassword").type(Cypress.env("LOGINPASSWORD"));
     cy.get("#loginForm > .modal-footer > .btn-success").click();
-    cy.wait(1000);
+    cy.wait(wait);
     cy.get(".profile-name").should("have.text", "testKit2");
   });
   it("it tries to login with invalid credentials", () => {
     cy.visit(url);
-    cy.wait(1000);
+    cy.wait(wait);
     cy.get("#registerForm > .modal-footer > .btn.btn-outline-success").click();
-    cy.wait(1000);
+    cy.wait(wait);
     cy.get("#loginEmail").type("testKit2.error@noroff.no");
     cy.get("#loginPassword").type("errorerror");
     cy.get("#loginForm > .modal-footer > .btn-success").click();
@@ -32,17 +34,17 @@ describe("LOGING TESTING", () => {
   });
   it("logs out of account", () => {
     cy.visit(url);
-    cy.wait(1000);
+    cy.wait(wait);
     cy.get("#registerForm > .modal-footer > .btn.btn-outline-success").click();
-    cy.wait(1000);
+    cy.wait(wait);
     cy.get("#loginEmail").type(Cypress.env("LOGINEMAIL"));
     cy.get("#loginPassword").type(Cypress.env("LOGINPASSWORD"));
     cy.get("#loginForm > .modal-footer > .btn-success").click();
-    cy.wait(1000).then(() => {
+    cy.wait(wait).then(() => {
       expect(localStorage.getItem("token").length).to.be.greaterThan(3);
     });
     cy.get('button[data-auth="logout"]').click();
-    cy.wait(1000).then(() => {
+    cy.wait(wait).then(() => {
       expect(localStorage.getItem("token")).to.equals(null);
 
       cy.get("#registerModalLabel").should("exist");
